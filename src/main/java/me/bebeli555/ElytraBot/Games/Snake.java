c6b86da1e783d421ebc36c5f92f3f0e61bf252c0
Natan <66911017+natan515@users.noreply.github.com>
2020-12-19 23:17:15 +0200

    Update Snake.java

--- src/main/java/me/bebeli555/ElytraBot/Games/Snake.java
+++ src/main/java/me/bebeli555/ElytraBot/Games/Snake.java
@@ -1,11 +1,7 @@
 package me.bebeli555.ElytraBot.Games;
 
-import java.util.ArrayList;
-import java.util.Objects;
-
 import org.lwjgl.input.Keyboard;
 import com.mojang.realmsclient.gui.ChatFormatting;
-
 import me.bebeli555.ElytraBot.Settings.Settings;
 import me.bebeli555.ElytraBot.Settings.SettingsInfo;
 import net.minecraft.client.Minecraft;
@@ -15,237 +11,292 @@ import net.minecraft.init.SoundEvents;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import scala.util.Random;
 
-public class Snake extends GuiScreen{
-	//Snake game made by: bebeli555
-
+public class Tetris extends GuiScreen{
+	//Tetris. Took a while to make so yall better play it!
+	
 	static Minecraft mc = Minecraft.getMinecraft();
-	static ArrayList<Integer> BodyX = new ArrayList<Integer>();
-	static ArrayList<Integer> BodyY = new ArrayList<Integer>();
-	static int SnakeSize = 0;
-	static int SnakeX = 0;
-	static int SnakeY = 0;
-	static int LastSnakeX = 0;
-	static int LastSnakeY = 0;
-	static int LastBodyX = 0;
-	static int LastBodyY = 0;
-	static int delay = 0;
-	static String status = "Right";
-	static boolean GameOver = true;
-	static int AppleX, AppleY;
+	static int fromX, toX;
+	static int fromY, toY;
+	static TetrisNode CurrentNode;
+	static int BeenDown = 0;
+	public static boolean GameOver = true;
+	public static int Score = 0;
 	static long lastSec = 0;
+	static long lastSecMove = 0;
+	
+	public static void DrawTetris(int drawX, int drawY, boolean DuelMode) {
+		//Set values
+		fromX = drawX;
+		toX = fromX + 150;
+		fromY = drawY;
+		toY = fromY + 250;
+		
+		//Draw main thingy.
+		drawRect(fromX, fromY, toX, toY, 0xFF000000);
 
-	public static void DrawSnake() {
-		//Draw the thingy rectangle and text
-		drawRect(150, 150, 350, 350, 0xFF000000);
 		GlStateManager.pushMatrix();
 		GlStateManager.scale(3.0F, 3.0F, 3.0F);
-		mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Snake", 70, 40, 0xffff);
+		mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Tetris", 143, 40, 0xffff);
 		GlStateManager.popMatrix();
 
-		//Draw personal best
+		// Draw personal best
 		GlStateManager.pushMatrix();
-		GlStateManager.scale(2.0F, 2.0F, 2.0F);
-		mc.fontRenderer.drawStringWithShadow(ChatFormatting.GOLD + "Personal Best: " + ChatFormatting.GREEN + (int)Settings.getDouble("SnakeBest"), 85, 180, 0xffff);
+		GlStateManager.scale(1.7F, 1.7F, 1.7F);
+		mc.fontRenderer.drawStringWithShadow(ChatFormatting.GOLD + "Personal Best: " + ChatFormatting.GREEN + (int) Settings.getDouble("TetrisBest"), 238, 240, 0xffff);
 		GlStateManager.popMatrix();
-
-		//Game over.
-		if (SnakeX < 140 || SnakeX > 340 || SnakeY < 140 || SnakeY > 340) {
-			GameOver();
+		
+		int Divided = 150 - (Score / 10);
+		if (Divided < 10) {
+			Divided = 10;
 		}
-
-		if (!GameOver) {
-			//Die if head is colliding in body
-			for (int i = 0; i < BodyX.size(); i++) {
-				if (BodyX.get(i) == SnakeX) {
-					if (BodyY.get(i) == SnakeY) {
-						GameOver();
-						return;
+		long sec = System.currentTimeMillis() / Divided;
+		if (sec != lastSec && !GameOver) {
+			//Create new node from the top.
+			if (TetrisNode.Nodes.isEmpty() || !CurrentNode.CanGoDown()) {
+				Score++;
+				BlockPos Player = new BlockPos(mc.player.posX, mc.player.posY, mc.player.posZ);
+				mc.world.playSound(Player, SoundEvents.BLOCK_GLASS_PLACE, SoundCategory.AMBIENT, 10222.5f, 1.5f, true);
+				RemoveLayer();
+				SetShapes();
+				
+				if (!CurrentNode.CanGoDown()) {
+					GameOver();
+				}
+			}
+			
+			if (CurrentNode.CanGoDown()) {
+				CurrentNode.MoveDown();
+			}
+			lastSec = sec;
+		}
+		
+		long sec2 = System.currentTimeMillis() / 40;
+		if (sec2 != lastSecMove) {
+			// Move automatically if holding key
+			if (!GameOver && CurrentNode != null) {
+				if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT) || Keyboard.isKeyDown(Keyboard.KEY_LEFT) || Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+					BeenDown++;
+					if (BeenDown > 3) {
+						if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+							if (CurrentNode.CanMoveRight()) {
+								CurrentNode.MoveRight();
+							}
+						} else if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+							if (CurrentNode.CanMoveLeft()) {
+								CurrentNode.MoveLeft();
+							}
+						} else {
+							if (CurrentNode.CanGoDown()) {
+								CurrentNode.MoveDown();
+							}
+						}
 					}
+				} else {
+					BeenDown = 0;
 				}
 			}
+			lastSecMove = sec2;
+		}
+		
+		//Draw the tetris blocks.
+		for (int i = 0; i < TetrisNode.Nodes.size(); i++) {
+			TetrisNode Node = TetrisNode.Nodes.get(i);
+			drawRect(Node.GetX(), Node.GetY(), Node.GetX() + TetrisNode.multiplier, Node.GetY() - TetrisNode.multiplier, Node.GetColor());
+		}
+		
+		//Draw white lines.
+		drawWhiteLine(fromX, toX, fromY, toY);
+		
+		
+		//Draw score
+		GlStateManager.pushMatrix();
+		GlStateManager.scale(1.25F, 1.25F, 1.25F);
+		mc.fontRenderer.drawStringWithShadow(ChatFormatting.DARK_AQUA + "Score: " + ChatFormatting.GREEN + Score, 325, 125, 0xffff);
+		GlStateManager.popMatrix();
+		
+		
+		if (GameOver) {
+			// GameOver Screen
+			GlStateManager.pushMatrix();
+			GlStateManager.scale(2.5F, 2.5F, 2.5F);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.RED + "Game Over!", 163, 70, 0xffff);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.RED + "Score = " + ChatFormatting.GREEN + Score, 165, 80, 0xffff);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.AQUA + "Controls:", 167, 95, 0xffff);
+			drawRect(165, 140, 215, 155, 0xFFFFFFFF);
+			GlStateManager.popMatrix();
 
-			//Draw score
-			mc.fontRenderer.drawStringWithShadow(ChatFormatting.RED + "Score = " + ChatFormatting.GREEN + SnakeSize, 158, 155, 0xffff);
+			GlStateManager.pushMatrix();
+			GlStateManager.scale(1.25F, 1.25F, 1.25F);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Arrow UP: " + ChatFormatting.DARK_AQUA + "Rotate", 335, 215, 0xffff);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Arrow Right: " + ChatFormatting.DARK_AQUA + "Move Right", 322, 225, 0xffff);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Arrow Left: " + ChatFormatting.DARK_AQUA + "Move Left", 325, 235, 0xffff);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Arrow Down: " + ChatFormatting.DARK_AQUA + "Drop Soft", 325, 245, 0xffff);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Space: " + ChatFormatting.DARK_AQUA + "Drop Hard", 335, 255, 0xffff);
+			GlStateManager.popMatrix();
 
-			// Generate apple
-			if (AppleX == 0 || AppleY == 0) {
-				GenerateApple();
+			GlStateManager.pushMatrix();
+			GlStateManager.scale(1.8F, 1.8F, 1.8F);
+			mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Click to play", 233, 200, 0xffff);
+			GlStateManager.popMatrix();
+			return;
+		}
+		
+		//Draw white marks at bottom.
+		for (int i = 0; i < CurrentNode.GetFamily().size(); i++) {
+			if (CurrentNode.CanGoDown()) {
+				CurrentNode.SetDownPosition();
+				TetrisNode Node = CurrentNode.GetFamily().get(i);
+				int X = Node.GetX();
+				int Y = Node.GetDownPosition();
+				drawRect(X, Y, X + 1, Y - TetrisNode.multiplier, 0xFFFFFFFF);
+				drawRect(X, Y, X + TetrisNode.multiplier, Y + 1, 0xFFFFFFFF);
+				drawRect(X + TetrisNode.multiplier, Y, X + TetrisNode.multiplier + 1, Y - TetrisNode.multiplier, 0xFFFFFFFF);
+				drawRect(X, Y - TetrisNode.multiplier, X + TetrisNode.multiplier, Y - TetrisNode.multiplier + 1, 0xFFFFFFFF);
 			}
-
-			// Draw apple
-			drawRect(AppleX, AppleY, AppleX + 20, AppleY + 20, 0xFFff0000);
-
-			long sec = System.currentTimeMillis() / 150;
-			if (sec != lastSec) {
-				delay = 0;
-				// Control snake movement
-				switch (status) {
-					case "Up":
-						SnakeY -= 20;
-						break;
-					case "Down":
-						SnakeY += 20;
-						break;
-					case "Right":
-						SnakeX += 20;
-						break;
-					case "Left":
-						SnakeX -= 20;
-						break;
-				}
-
-				if (!BodyX.isEmpty()) {
-					BodyX.remove(BodyX.get(0));
-					BodyY.remove(BodyY.get(0));
-					BodyX.add(LastSnakeX);
-					BodyY.add(LastSnakeY);
-				}
-				lastSec = sec;
-			}
-
-			//Eat apple
-			if (SnakeX == AppleX) {
-				if (SnakeY == AppleY) {
-					SnakeSize++;
-					AppleX = 0;
-					AppleY = 0;
-
-					//More body for snake bcs he fat and eating all those sugary apples
-					if (BodyX.isEmpty()) {
-						BodyX.add(LastSnakeX);
-						BodyY.add(LastSnakeY);
-					} else {
-						BodyX.add(LastBodyX);
-						BodyY.add(LastBodyY);
+		}
+	}
+	
+	//Set a random shape.
+	public static void SetShapes() {
+		TetrisNode n = new TetrisNode(fromX + 50, fromY);
+		n.AddToFamily(n);
+		int x = n.GetX();
+		int y = n.GetY();
+		
+		Random rand = new Random();
+		int random = rand.nextInt(7);
+		if (random == 0) {
+			n.AddToFamily(new TetrisNode(x + 10, y));
+			n.AddToFamily(new TetrisNode(x, y + 10));
+			n.AddToFamily(new TetrisNode(x + 10, y + 10));
+			n.SetShape("O");
+			n.SetColor(0xFFFFFF00);
+		} else if (random == 1) {
+			n.AddToFamily(new TetrisNode(x, y + 10));
+			n.AddToFamily(new TetrisNode(x, y + 20));
+			n.AddToFamily(new TetrisNode(x, y + 30));
+			n.SetShape("I");
+			n.SetColor(0xFF36EAFF);
+		} else if (random == 2) {
+			n.AddToFamily(new TetrisNode(x + 10, y));
+			n.AddToFamily(new TetrisNode(x, y + 10));
+			n.AddToFamily(new TetrisNode(x - 10, y + 10));
+			n.SetShape("S");
+			n.SetColor(0xFFFF0009);
+		} else if (random == 3) {
+			n.AddToFamily(new TetrisNode(x - 10, y));
+			n.AddToFamily(new TetrisNode(x, y + 10));
+			n.AddToFamily(new TetrisNode(x + 10, y + 10));
+			n.SetShape("Z");
+			n.SetColor(0xFF00FF2B);
+		} else if (random == 4) {
+			n.AddToFamily(new TetrisNode(x, y + 10));
+			n.AddToFamily(new TetrisNode(x, y + 20));
+			n.AddToFamily(new TetrisNode(x + 10, y + 20));
+			n.SetShape("L");
+			n.SetColor(0xFFEC830C);
+		} else if (random == 5) {
+			n.AddToFamily(new TetrisNode(x, y + 10));
+			n.AddToFamily(new TetrisNode(x, y + 20));
+			n.AddToFamily(new TetrisNode(x - 10, y + 20));
+			n.SetShape("J");
+			n.SetColor(0xFFFF19EF);
+		} else {
+			n.AddToFamily(new TetrisNode(x + 10, y));
+			n.AddToFamily(new TetrisNode(x - 10, y));
+			n.AddToFamily(new TetrisNode(x, y + 10));
+			n.SetShape("T");
+			n.SetColor(0xFF9100FF);
+		}
+		
+		CurrentNode = n;
+	}
+	
+	public static void RemoveLayer() {
+		if (CurrentNode == null) {
+			return;
+		}
+		
+		for (int i = 0; i < CurrentNode.GetFamily().size(); i++) {
+			int y = CurrentNode.GetFamily().get(i).GetY();
+			int x = fromX - 10;
+			for (int i2 = 0; i2 < 100; i2++) {
+				x = x + 10;
+				if (x > toX - 10) {
+					x = fromX - 10;
+					for (int i3 = 0; i3 < 100; i3++) {
+						x = x + 10;
+						TetrisNode.Nodes.remove(TetrisNode.GetNode(x, y));
+					}
+					for (int i4 = 0; i4 < TetrisNode.Nodes.size(); i4++) {
+						if (TetrisNode.Nodes.get(i4).GetY() <= y) {
+							TetrisNode.Nodes.get(i4).SetY(TetrisNode.Nodes.get(i4).GetY() + 10);
+						}
 					}
 					BlockPos Player = new BlockPos(mc.player.posX, mc.player.posY, mc.player.posZ);
-					mc.world.playSound(Player, SoundEvents.ENTITY_PLAYER_LEVELUP, SoundCategory.AMBIENT, 100.0f, 2.0F, true);
+					mc.world.playSound(Player, SoundEvents.BLOCK_ANVIL_USE, SoundCategory.AMBIENT, 10222.5f, 1.5f, true);
+					Score = Score + 5;
+					break;
+				}
+				
+				if (TetrisNode.GetNode(x, y) == null) {
+					break;
 				}
 			}
-
-			LastSnakeX = SnakeX;
-			LastSnakeY = SnakeY;
-			if (!BodyX.isEmpty()) {
-				LastBodyX = BodyX.get(BodyX.size() - 1);
-				LastBodyY = BodyY.get(BodyY.size() - 1);
-			}
-
-			// Draw snake
-			drawRect(SnakeX, SnakeY, SnakeX + 20, SnakeY + 20, 0xFF55ff00);
-			drawRect(SnakeX + 3, SnakeY + 3, SnakeX + 8, SnakeY + 8, 0xFF000000);
-			for (int i = 0; i < BodyX.size(); i++) {
-				drawRect(BodyX.get(i), BodyY.get(i), BodyX.get(i) + 20, BodyY.get(i) + 20, 0xFF55ff00);
-			}
-		}
-
-		//Draw white lines
-		for (int i = 0; i < 10; i++) {
-			GlStateManager.pushMatrix();
-			GlStateManager.scale(0.25f, 0.25f, 0.25f);
-			drawRect(600, 150 * 4,602, 350 * 4, 0xFFFFFFFF);
-			drawRect(680 + ((i * 20) * 4), 150 *4, 682 + ((i * 20) * 4), 350 *4, 0xFFFFFFFF);
-			GlStateManager.popMatrix();
-		}
-
-		//Other lines This is big math time.
-		for (int i = 0; i < 11; i++) {
-			GlStateManager.pushMatrix();
-			GlStateManager.scale(0.25f, 0.25f, 0.25f);
-			drawRect(150 * 4, 600 + ((i * 20) * 4), 350 * 4, 602 + ((i * 20) * 4), 0xFFFFFFFF);
-			GlStateManager.popMatrix();
-		}
-
-		//Game over screen
-		if (GameOver) {
-			GlStateManager.pushMatrix();
-			GlStateManager.scale(3.0F, 3.0F, 3.0F);
-			mc.fontRenderer.drawStringWithShadow(ChatFormatting.RED + "Game Over!", 56, 63, 0xffff);
-			mc.fontRenderer.drawStringWithShadow(ChatFormatting.RED + "Score = " + ChatFormatting.GREEN + SnakeSize, 59, 80, 0xffff);
-			GlStateManager.popMatrix();
-			drawRect(170, 290, 330, 330, 0xFFFFFFFF);
-			GlStateManager.pushMatrix();
-			GlStateManager.scale(2.0F, 2.0F, 2.0F);
-			mc.fontRenderer.drawStringWithShadow(ChatFormatting.GREEN + "Click to Play!", 92, 150, 0xffff);
-			GlStateManager.popMatrix();
-			GlStateManager.pushMatrix();
-			GlStateManager.scale(1.5F, 1.5F, 1.5F);
-			mc.fontRenderer.drawStringWithShadow(ChatFormatting.AQUA + "Use " + ChatFormatting.GREEN + "ARROW KEYS " + ChatFormatting.AQUA + "To play!", 107, 110, 0xffff);
-			GlStateManager.popMatrix();
 		}
 	}
-
-	public static void OnClick(int i, int j) {
-		//Start game 150, 150, 350, 350
-		if (150 < i && 350 > i && 150 < j && 350 > j)
-			if (GameOver) StartGame();
-	}
-
-	public static void OnClick(GuiScreenEvent.KeyboardInputEvent.Post e) {
-
-		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) status = "Down";
-
-		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) status = "Up";
-
-		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) status = "Right";
-
-		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) status = "Left";
-	}
-
+	
 	public static void GameOver() {
-		if (!GameOver)
-			if (Objects.requireNonNull(SettingsInfo.getSetting("SnakeBest")).value == null || Integer.parseInt(String.valueOf(Objects.requireNonNull(SettingsInfo.getSetting("SnakeBest")).value)) < SnakeSize) {
-				Objects.requireNonNull(SettingsInfo.getSetting("SnakeBest")).value = SnakeSize;
-				Settings.WriteSettings();
-			}
-			GameOver = true;
-			BodyX.clear();
-			BodyY.clear();
-			LastSnakeX = 0;
-			LastSnakeY = 0;
-			AppleX = 0;
-			AppleY = 0;
+		if (SettingsInfo.getSetting("TetrisBest").value == null || Integer.parseInt(String.valueOf(SettingsInfo.getSetting("TetrisBest").value)) < Score) {
+			SettingsInfo.getSetting("TetrisBest").value = Score;
+			Settings.WriteSettings();
 		}
+		GameOver = true;
+		TetrisNode.Nodes.clear();
+	}
 
 	public static void StartGame() {
 		GameOver = false;
-		SnakeX = 230;
-		SnakeY = 330;
-		status = "Up";
-		SnakeSize = 1;
+		Score = 0;
 	}
-
-	public static void GenerateApple() {
-		for (int i = 0; i < 100; i++) {
-			Random rand = new Random();
-			int random = rand.nextInt(10);
-			int random2 = rand.nextInt(10);
-			AppleX = 150 + random * 20;
-			AppleY = 150 + random2 * 20;
-
-			for (int i2 = 0; i2 < BodyX.size(); i2++) {
-				if (BodyX.get(i2) == AppleX) {
-					if (BodyY.get(i2) == AppleY) {
-						AppleX = 0;
-						AppleY = 0;
-						break;
-					}
-				}
+	
+	public static void drawWhiteLine(int x, int x2, int y, int y2) {
+		GlStateManager.pushMatrix();
+		GlStateManager.scale(0.25f, 0.25f, 0.25f);
+		int Thisx = x * 4; int Thisy = y * 4; int Thisx2 = x2 * 4; int Thisy2 = y2 * 4;
+		drawRect(Thisx, Thisy, Thisx + 2, Thisy2, 0xFFFFFFFF);
+		drawRect(Thisx2, Thisy, Thisx2 - 2, Thisy2, 0xFFFFFFFF);
+		drawRect(Thisx, Thisy, Thisx2, Thisy + 2, 0xFFFFFFFF);
+		drawRect(Thisx, Thisy2, Thisx2, Thisy2 + 2, 0xFFFFFFFF);
+		GlStateManager.popMatrix();
+	}
+	
+	@SubscribeEvent
+	public void OnKeyInput(GuiScreenEvent.KeyboardInputEvent.Post e) {
+		//Control tetris block movement
+		if (CurrentNode == null || GameOver) {
+			return;
+		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+			if (CurrentNode.CanMoveRight()) {
+				CurrentNode.MoveRight();
 			}
-
-			if (SnakeX == AppleX && SnakeY == AppleY) {
-				AppleX = 0;
-				AppleY = 0;
-				continue;
+		} else if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+			if (CurrentNode.CanMoveLeft()) {
+				CurrentNode.MoveLeft();
 			}
-
-			if (AppleX < 140 || AppleX > 340 || AppleY < 140 || AppleY > 340) {
-				AppleX = 0;
-				AppleY = 0;
-				continue;
+		} else if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			CurrentNode.MoveCompletelyDown();
+		} else if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+			CurrentNode.Rotate();
+		} else if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+			if (CurrentNode.CanGoDown()) {
+				CurrentNode.MoveDown();
 			}
-			break;
 		}
 	}
 }
